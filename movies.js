7d6e0427de4099d54807a2dfb00732f4ab296b12
T.K <nique@niques-air.myfiosgateway.com>
2022-04-23 16:05:01 -0400

    14 pass

--- movies.js
+++ movies.js
@@ -1,10 +1,27 @@
+// import films from "./cypress/support/intercept/films";
+
+// import films from "./cypress/support/intercept/films";
+
 console.log('This is connected');
+
+const appState = {
+    filmsData: [],
+}
 
 const BASE_URL = "https://ghibliapi.herokuapp.com/films/";
 
-const getMovies = () => {
+const getFilms = () => {
     console.log('This is working');
     fetch (BASE_URL)
         .then(data => data.json())
-        .then(movies => console.log(movies))
+        .then(json => {
+            appState.filmsData = json;
+            console.log(appState.filmsData)
+        })
 }
+setTimeout(getFilms, 1000);
+
+// const select = document.getElementById('titles');
+// const option = document.createElement('option')
+// option.innerHTML = `${appState.filmsData[0].title[0]}`;
+// select.appendChild(option)

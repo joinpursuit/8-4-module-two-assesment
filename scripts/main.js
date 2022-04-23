0bd3de3bead621789b4c40e608f9a8fd63ceae3f
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-04-23 11:05:36 -0400

    movie list populated, movie description working

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+//ENV variables
+const API = 'https://ghibliapi.herokuapp.com';
+
+//global html elements
+const movieList = document.getElementById('movieList');
+const movieDetails = document.getElementById('display-info');
+
+//event listeners
+movieList.addEventListener('change', (e) => changeMovie(e));
+
+fetch(`${API}/films`)
+    .then(res => res.json())
+    .then(res => populateList(res))
+    .catch(err => alert(err));
+
+const populateList = (res) => {
+    for (let movie of res) {
+        const newMovie = document.createElement('option')
+        newMovie.setAttribute('value', movie.id);
+        newMovie.textContent = movie.title;
+        movieList.append(newMovie);
+    }
+}
+
+const changeMovie = (e) => {
+    console.log (e.target.value);
+    fetch(`${API}/films/${e.target.value}`)
+        .then(res => res.json())
+        .then(res => {
+            const header = document.createElement('h3');
+            header.textContent = res.title;
+
+            const year = document.createElement('p');
+            year.textContent = res.release_date;
+
+            const description = document.createElement('p');
+            description.textContent = res.description;
+
+            movieDetails.append(header, year, description);
+
+        })
+        .catch(err => alert(err));
+
+}
+

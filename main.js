bd2bcb1d9a8502a7dae032e68d18fb814c5ae24d
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-04-23 13:16:44 -0400

    "break"

---
+++ main.js
@@ -0,0 +1,13 @@
+const img = document.querySelector("img");
+img.setAttribute("src","./images/ghibli-logo.png",);
+img.setAttribute("alt", "Ghibli logo")
+
+const sectionReview = document.getElementById("reviews");
+sectionReview.style.display = "block";
+
+
+const selectForm = document.getElementById("selectMovie")
+// selectForm.addEventListener
+
+const BASE_URL = "https://ghibliapi.herokuapp.com";
+

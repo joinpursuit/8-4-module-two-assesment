6560792b3fa814c4b288aec4a45e304505ee371a
Samantha Figueroa <samanthafigueroa@pursuit.org>
2022-04-23 18:03:08 -0400

    add additional code for get people

--- main.js
+++ main.js
@@ -7,25 +7,32 @@ function getMovies() {
     .then((res) => res.json())
     .then((data) => {
       movies = data;
-    //   console.log(movies);
+      // console.log(movies);
       getTitles(movies);
     });
 }
 
+//enclose getMovies function in a setTimeout so that getTitles runs first and populates the select with options
 setTimeout(getMovies, 1000);
 
 const dropDown = document.querySelector("select");
 
+//this function will add all the movie titles as options into the select tag
 function getTitles(movies) {
   for (let i = 0; i < movies.length; i++) {
     dropDown.innerHTML += `<option value=${movies[i].id}>${movies[i].title}</option>`;
-  };
-};
+  }
+}
 
+//create the different elements of the movie description
 const title = document.createElement("h3");
 const year = document.createElement("p");
 const description = document.createElement("p");
 const displayArea = document.querySelector("#display-info");
+
+//when a movie is selected, add the relevant contents to the display area
+
+let optionsSelected = [];
 
 dropDown.addEventListener("change", (event) => {
   movies.filter((el) => {
@@ -34,24 +41,72 @@ dropDown.addEventListener("change", (event) => {
       year.textContent = el.release_date;
       description.textContent = el.description;
       displayArea.append(title, year, description);
-    };
+      optionsSelected.push(event.target.value);
+      // console.log(optionsSelected)
+    }
   });
 });
 
-const form = document.querySelector('form');
-const reviewList = document.querySelector('#reviews ul');
+//call the review form and ul list
+const form = document.querySelector("form");
+const reviewList = document.querySelector("#reviews ul");
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const userInput = event.target.review.value;
+//when a review is submitted the value(text) is saved to a variable and used to populate the li elements
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const userInput = event.target.review.value;
 
-    movies.filter((el) => {
-        if( title.textContent === el.title){
-            const listItem = document.createElement('li');
-            listItem.innerHTML = `<strong>${el.title}:</strong> ${userInput}`
-            reviewList.append(listItem);
-        };
+  //does not pass cypress
+  if (!title.textContent) {
+    alert("Please select a movie first");
+  }
+
+  movies.filter((el) => {
+    if (title.textContent === el.title) {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = `<strong>${el.title}:</strong> ${userInput}`;
+      reviewList.append(listItem);
+      form.reset();
+    }
+  });
+});
+
+//call the reset button and remove all elements inside the ul
+const resetButton = document.querySelector("#reset-reviews");
+
+resetButton.addEventListener("click", () => {
+  reviewList.remove();
+});
+
+///////SHOW PEOPLE////////
+let people;
+
+const peopleAPI = "https://ghibliapi.herokuapp.com/people";
+
+function getPeople() {
+  fetch(peopleAPI)
+    .then((res) => res.json())
+    .then((data) => {
+      people = data;
+      //   console.log(people);
     });
+}
 
-    form.reset();
+getPeople();
+
+const showPeople = document.querySelector("#show-people");
+const peopleList = document.querySelector("section ol");
+
+showPeople.addEventListener("click", () => {
+  for (let option of optionsSelected) {
+    people.filter((person) => {
+      person.films.filter((link) => {
+        if (link.includes(option)) {
+          const numberedItem = document.createElement("li");
+          peopleList.append(numberedItem);
+          numberedItem.textContent = person.name;
+        }
+      });
+    });
+  }
 });

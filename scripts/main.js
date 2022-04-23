747e665f527883132fc915a56aab7b397b67abfe
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-04-23 11:44:47 -0400

    reviews section working

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,14 @@ const API = 'https://ghibliapi.herokuapp.com';
 //global html elements
 const movieList = document.getElementById('movieList');
 const movieDetails = document.getElementById('display-info');
+const reviewForm = document.getElementById('reviewForm');
+const reviewList = document.querySelector('#reviews ul');
+const resetButton = document.getElementById('reset-reviews');
 
 //event listeners
 movieList.addEventListener('change', (e) => changeMovie(e));
+reviewForm.addEventListener('submit', (e) => submitReview(e));
+resetButton.addEventListener('click', (e) => resetReviews());
 
 fetch(`${API}/films`)
     .then(res => res.json())
@@ -24,7 +29,7 @@ const populateList = (res) => {
 
 const changeMovie = (e) => {
     movieDetails.innerHTML = ''; //ideally this would be a loop to remove all children
-    
+
     fetch(`${API}/films/${e.target.value}`)
         .then(res => res.json())
         .then(res => {
@@ -43,4 +48,25 @@ const changeMovie = (e) => {
         .catch(err => alert(err));
 
 }
+
+const submitReview = (e) => {
+    e.preventDefault();
+    const review = e.target.review.value;
+    const title = document.querySelector('#display-info h3').textContent;
+    e.target.review.value = '';
+
+    const strong = document.createElement('strong');
+    strong.textContent = `${title}: `;
+
+    const reviewLi = document.createElement('li');
+
+    reviewLi.append(strong);
+    reviewLi.append(review);
+    
+    reviewList.append(reviewLi);
+}
+
+const resetReviews = (e) => reviewList.innerHTML = '';
+
+
 

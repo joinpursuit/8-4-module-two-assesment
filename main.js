93dd151f560db9871c80e90cd93cb5cca690dcfd
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-04-23 16:15:06 -0400

    "fetch"

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const BASE_URL = "https://ghibliapi.herokuapp.com";
+
+
 const img = document.querySelector("img");
 img.setAttribute("src","./images/ghibli-logo.png",);
 img.setAttribute("alt", "Ghibli logo")
@@ -5,9 +8,35 @@ img.setAttribute("alt", "Ghibli logo")
 const sectionReview = document.getElementById("reviews");
 sectionReview.style.display = "block";
 
+const option = document.querySelector("option");
+option.value = "";
+const dropDown = document.getElementById("dropDown");
+// dropDown.empty();
 
-const selectForm = document.getElementById("selectMovie")
-// selectForm.addEventListener
+let url = "https://ghibliapi.herokuapp.com/films";
 
-const BASE_URL = "https://ghibliapi.herokuapp.com";
+fetch(url)
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((error) =>console.log("Must select movie", error))
+
+
+
+// function getMovies (data){
+//     for(let i = 0; i < data.length; i++){
+//         option.value = data[i].id;
+//         option.textContent = data[i].title
+        
+    
+
+
+
+
+
+
+
+
+
+
+
 

4636bf7b1d4a6a2199497b6957c536d228cc11b5
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-04-24 15:38:02 -0400

    "almost"

--- main.js
+++ main.js
@@ -1,59 +1,17 @@
 
-const BASE_URL = "https://ghibliapi.herokuapp.com/films";
-getMovies();
 
-const img = document.querySelector("img");
-img.setAttribute("src","./images/ghibli-logo.png",);
-img.setAttribute("alt", "Ghibli logo")
+const API = 'https://ghibliapi.herokuapp.com';
 
 
-const select = document.createElement("select")
-const option = document.createElement("option")
+let currentdata;
 
 
-
-function getMovies (){
-
-    fetch(BASE_URL)
-            .then((res) => res.json())
-            .then((data) => selectMenu(data))
-            .catch((error) =>console.log("Must select movie", error))
-}
+    fetch(`${API}/films`)
+            .then(res => res.json())
+            .then(res => populateList(res))
+            .catch(err => alert(err));
 
 
-function selectMenu (data){
-    for(let i = 0; i < data.length; i++){
-        const select = document.querySelector("select");
-        const option = document.createElement("option");
-
-        option.value = data[i].id;
-        option.textContent = data[i].title
-        select.append(option);
-    } 
-};
-
-
-const selectForm = document.querySelector("form")
-console.log(selectForm);
-selectForm.addEventListener("select", event =>{
-    event.preventDefault()
-
-    if(option.value === data.id){
-        const h3 = document.querySelector("h3");
-        h3.textContent= data.title
-        const p1 = document.querySelector("p")
-        p1.textContent = data.release_date
-        const p2 = document.querySelector("p")
-        p2.textContent = data.desrciption
-        const display = document.getElementById("display-info");
-        display.append(h3)
-    }else{
-        alert("Must select a movie")
-    }
-
-
-
-} )
 
 
 

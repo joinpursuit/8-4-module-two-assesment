a245dbc9ead6e27ef992b777964fd4ac8a1e7181
T.K <nique@Niques-MacBook-Air.local>
2022-04-24 16:42:00 -0400

    18 pass

--- movies.js
+++ movies.js
@@ -2,11 +2,13 @@
 
 // import films from "./cypress/support/intercept/films";
 
+// import films from "./cypress/support/intercept/films";
+
 console.log('This is connected');
 
 const appState = {
-    filmsData: [],
-}
+    filmsData: []
+};
 
 const BASE_URL = "https://ghibliapi.herokuapp.com/films/";
 
@@ -14,14 +16,40 @@ const getFilms = () => {
     console.log('This is working');
     fetch (BASE_URL)
         .then(data => data.json())
-        .then(json => {
-            appState.filmsData = json;
-            console.log(appState.filmsData)
+        .then(movies => {
+            appState.filmsData = movies;
+            populateSelectbox();
+            createMovieDetails();
         })
-}
+};
 setTimeout(getFilms, 1000);
 
-// const select = document.getElementById('titles');
-// const option = document.createElement('option')
-// option.innerHTML = `${appState.filmsData[0].title[0]}`;
-// select.appendChild(option)
+const select = document.getElementById('titles');
+const h3 = document.querySelector('#title');
+const p1 = document.querySelector('#release-year');
+const p2 = document.querySelector('#description');
+
+const populateSelectbox = () => {
+    appState.filmsData.forEach(film => {
+        const option = document.createElement('option');
+        option.value = `${film.id}`;
+        option.innerHTML = `${film.title}`;
+        select.appendChild(option);
+        // console.log(film)
+    });
+};
+
+const createMovieDetails = () => {
+    select.addEventListener('change', (e) => {
+        console.log(e.target.value)
+        e.preventDefault();
+        fetch(`${BASE_URL}${select.value}`)
+            .then(data => data.json())
+            .then(data => {
+                h3.innerHTML = `${data.title}`
+                p1.innerHTML = `${data.release_date}`
+                p2.innerHTML = `${data.description}`
+        });
+    });
+};
+

2e006bfd26ea63ac5bfd08fd407ab91b4c593c93
Denisse Maldonado <denissemaldonado@Denisses-MacBook-Air.local>
2022-04-24 13:43:23 -0400

    18/20 tests pass

--- index.js
+++ index.js
@@ -17,97 +17,117 @@ function getFilms() {
 }
 getFilms();
 
-
+// ----------------------------- Select a Movie ----------------------------- //
 function selectFilms(allFilms) {
     const section = document.querySelector('#titles');
     allFilms.filter(films => {
         section.innerHTML += `<option value="${films.title}">${films.title}</option>`
     })
     section.addEventListener('change', (e) => {
-        if (document.querySelector('#titles option') !== '') {
-            movieTitle = e.target.value;
-            movieDescription(e.target.value);
-            const list = document.querySelectorAll('#People li');
-            list.forEach(li => li.remove())
-        }
-    });
 
+        movieTitle = e.target.value;
+        movieDescription(movieTitle);
+        // Helps remove 'People' listed
+        const list = document.querySelectorAll('#People li');
+        list.forEach(li => li.remove())
+        
+        // Helps reset 'People' list :
+        clickNumber = 0;
+        document.getElementById('show-people').disabled = false;
+    });
 }
 
+function movieError(){
+    window.alert('Please select a movie first');
+}
+
+// ----------------------------- Movie Description ----------------------------- //
+const sectionDiv = document.querySelector('#display-info')
 function movieDescription(title) {
-    console.log(title);
-    const sectionDiv = document.querySelector('#display-info')
+    if(title === ''){
+        sectionDiv.innerHTML = '';
+    } 
     allFilms.filter((film, i) => {
         if (film.title === title) {
             sectionDiv.innerHTML = `<h2>${title}</h2><p>${allFilms[i]['release_date']}</p><p>${allFilms[i]['description']}</p>`;
-            movieArry = allFilms[i]['people'];
-        }
+            movieArry = allFilms[i];
+        } 
 
     })
     addReviews(title);
 }
 
-
+// ----------------------------- Addding Reviews ----------------------------- //
 const sectionForm = document.querySelector('#Add-review form');
 sectionForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log(event.target.review.value);
-    const input = event.target.review.value;
-    allReviews[movieTitle] = input;
-    addReviews(input);
-    sectionForm.reset();
+    console.log(movieTitle)
+    if (movieTitle === undefined || !movieTitle){
+        movieError();
+        console.log('this picks up empty value');
+    } else {
+        console.log(event.target.review.value);
+        const input = event.target.review.value;
+        allReviews[movieTitle] = input;
+        addReviews(input);
+        sectionForm.reset();
+    } 
+    
 });
 
 
-function addReviews(title) {
+function addReviews() {
     const sectionReviews = document.querySelector('#reviews ul')
     if (Object.keys(allReviews).length) {
         sectionListItems = document.querySelectorAll('li')
         sectionListItems.forEach(x => x.remove());
-
+        
         for (const [key, value] of Object.entries(allReviews)) {
-            sectionReviews.innerHTML += `<li><strong>|<b>${key}</strong>|</b>: ${value}</li>`
+            sectionReviews.innerHTML += `<li><strong><b>${key}</strong></b>: ${value}</li>`
         }
-    }
+    } 
 }
 
-function deleteReviews(){
-    console.log('im here bitch')
+// ----------------------------- Error Message ----------------------------- //
+
+
+// ----------------------------- Deletes Reviews ----------------------------- //
+function deleteReviews() {
     const listedReviews = document.querySelector('#reviews ul')
     listedReviews.innerHTML = '';
 }
 
-
-
 // ----------------------------- People Listed ----------------------------- //
 let movieArry;
-console.log(movieArry)
 
-const sectionPeople = document.querySelector('#People');
+const sectionPeople = document.querySelector('#People button');
+let clickNumber = 0;
 sectionPeople.addEventListener("click", (e) => {
-    movieArry.forEach(x => {
-        console.log(x)
-        getPeopleInfo(x);
-    })
-    document.getElementById('show-people').disabled = true;
+    clickNumber += 1;
+    if (movieTitle === movieArry['title']){
+        if(clickNumber === 1){
+            getPeopleInfo('https://ghibliapi.herokuapp.com/people', movieArry['url']);
+        } if (clickNumber >= 2){
+            document.getElementById('show-people').disabled = true;
+        }
+    }
+    console.log(clickNumber)
 })
 
-function getPeopleInfo(x) {
-    const sectionOL = document.querySelector('#People ol')
-    fetch(x)
+function getPeopleInfo(peopleURL, url) {
+    const sectionOL = document.querySelector('#People ol');
+    fetch(peopleURL)
         .then((res) => res.json())
         .then((data) => {
-            if (data.length > 1) { // For multiple names listed in an object
-                data.forEach(x => {
-                    sectionOL.innerHTML += `<li>${x.name}</li>`
-                })
-            } else {  // For one name listed at a time
-                sectionOL.innerHTML += `<li>${data.name}</li>`
+            console.log(data)
+            for (let i = 0; i < data.length; i++) {
+                filmInfo = data[i]['films'];
+                console.log(filmInfo)
+                if (filmInfo.includes(url)) {
+                    sectionOL.innerHTML += `<li>${data[i].name}</li>`
+                }
             }
         })
+        
+
 }
-
-
-
-
-

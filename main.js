724ccfd369bff9edd56a40147dee97cbf016ce72
John Prado <jcprado20@icloud.com>
2022-04-24 16:42:08 -0400

    add some functionality

--- main.js
+++ main.js
@@ -3,40 +3,66 @@ const movieInfo = document.querySelector("#display-info");
 const title = document.querySelector("#title");
 const releaseYear = document.querySelector("#release-year");
 const description = document.querySelector("#description");
-const review = document.querySelector("#review-form")
-const reviews = document.querySelector("#review-list")
+const review = document.querySelector("#review-form");
+const reviews = document.querySelector("#review-list");
+const peopleSection = document.querySelector("#people-section");
+const peopleList = document.querySelector("#people-list");
 
 
-fetch("https://ghibliapi.herokuapp.com/films")
-    .then(response => response.json())
-    .then(response => {
+const getData = () => {
+    fetch("https://ghibliapi.herokuapp.com/films")
+      .then((response) => response.json())
+      .then((response) => {
         response.forEach((movie) => {
-            const option = document.createElement("option");
-            option.setAttribute("value", movie.id);
-            option.innerHTML = movie.title;
-            movies.append(option);
+          const option = document.createElement("option");
+          option.setAttribute("value", movie.id);
+          option.innerHTML = movie.title;
+          movies.append(option);
         });
-    })
+      });  
+}
 
 movies.addEventListener("change", (e) => {
-    e.preventDefault();
-    fetch(`https://ghibliapi.herokuapp.com/films/${movies.value}`)
-    .then(response => response.json())
-    .then( response => {
-        title.innerHTML = response.title;
-        releaseYear.innerHTML = response.release_date;
-        description.innerHTML = response.description;
-    })
-})
+  e.preventDefault();
+  fetch(`https://ghibliapi.herokuapp.com/films/${movies.value}`)
+    .then((response) => response.json())
+    .then((response) => {
+      title.innerHTML = response.title;
+      releaseYear.innerHTML = response.release_date;
+      description.innerHTML = response.description;
+    });
+});
 
 review.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let name = e.target.review.value;
+  if (name) {
+    const reviewItem = document.createElement("li");
+    reviewItem.innerHTML = `<strong>${title.innerHTML}: </strong>${name}`;
+    reviews.append(reviewItem);
+  }
+  review.reset();
+});
+
+peopleSection.addEventListener("click", (e) => {
+  e.preventDefault();
+  fetch("https://ghibliapi.herokuapp.com/people/")
+    .then((response) => response.json())
+    .then((response) => {
+      let persons = response.filter((person) => {
+          return person.id = option.value});
+      persons.forEach((person) => {
+        let character = document.createElement("li");
+        character.innerHTML = person.name;
+        peopleList.append(character);
+      });
+    });
+});
+
+reviews.addEventListener("click", (e) => {
     e.preventDefault();
-    let name = e.target.review.value;
-    if(name){
-        const reviewItem = document.createElement("li");
-        reviewItem.innerHTML = `<strong>${title.innerHTML}: </strong>${name}`;
-        reviews.append(reviewItem)
-    }
-
+    const allReviews = document.querySelectorAll("li");
+    allReviews.forEach(review => {
+        review.remove()});
 })
-
+setTimeout(getData, 1000)

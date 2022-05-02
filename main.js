5859732759b19ce3f4534e5b90b275076c1c86b2
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-05-02 12:13:45 -0400

    "redo"

--- main.js
+++ main.js
@@ -17,8 +17,9 @@ document.getElementById("titles").addEventListener("change", (e) => {
     // create a variable for when select option changes
     const id = e.target.value
 
-    // when select option changes run the updateSelected function with the movie id
-    updateSelected(id)
+    // when select option changes run the updateSelected function with matching movie id and add the selected movie to appState select movies object template
+    appState.selectMovies = updateSelected(id)
+    setMovieDescription()
 
 })
 
@@ -27,23 +28,25 @@ document.getElementById("titles").addEventListener("change", (e) => {
 
 
 
-// FETCH - wrap API call in HELPER FUNCTION then call the function 
+// FETCH - wrap API call in HELPER FUNCTION then call the function at the end with SetTimeout for async
 //get the data from the Base URL path of films - ENDPOINT
 const getMovies = () => {
-    // test to see if function is fetching information 
+    // TEST - function is fetching info
     console.log("Getting API data")
+    // fetch data from Base URL with the path of films
     fetch(BASE_URL + "films")
     .then(res => res.json())
     .then(data => { 
         //console.log(data) to make sure you are getting the right info back
         // assigning API data to appState template movies array 
         appState.movies = data
+        // call the function populateSelect to add API movies to select tag as options
         populateSelect(appState.movies)
     })  
 }
 // SELECT - populate options tag w/ movie titles from API call  - options tag should have a value of movie id and text of movie title - first option should be blank
 
-// create a HELPER FUNCTION then add that function to the fetch with appState.movies parameter
+// create a HELPER FUNCTION to create select options  the fetch with appState.movies parameter
 const populateSelect = (movies) => { 
     // TEST - movies will populate 
     console.log("Populate select options")
@@ -59,17 +62,34 @@ const populateSelect = (movies) => {
     
 }
 
-// HELPER FUNCTION to update the select options with movie id
+// HELPER FUNCTION to update the select options with movie id for the Event Listener
 const updateSelected = (id) => {
     // TEST - when new option is selected
     console.log("Update select options")
     // filter through appState movies and return the movie id that matches the id
-    return appState.movies.filter(movie =>{
+    return appState.movies.filter(movie => {
         return movie.id === id
     })
 }
 
-// set time out to run the function after 1000m/s to avoid async error
+// HELPER FUNCTION  to get the movie desciptions for H3 and (2) P tags
+const setMovieDescription = () => {
+// TEST - select event listener
+alert(appState.selectMovies.title)
+
+// creating variables for the description information to the appState selectMovies object template
+const { title, release_date, description } = appState.selectMovies
+
+// creating the HTML tags with interpolation to a variable template 
+const template = 
+`<h3>${title}</h3><p>${release_date}</p><p>${description}</p>`
+
+
+}
+
+
+
+// set time out to run the getMovies function after 1000m/s to avoid async error
 setTimeout(getMovies, 1000)
 
 
